6c0447afade4fbb229f6beecae5ceb4281de1b3e
Giorgi Zautashvili <Giorgi Zautashvili>
2016-08-10 22:38:05 +0200

    add inner component via template

--- APM - Start/app/app.component.ts
+++ APM - Start/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from 'angular2/core'
+import { ProductListComponent } from './products/product-list.component'
 
 @Component({
 
@@ -6,9 +7,10 @@ import { Component } from 'angular2/core'
     template: `
         <div>
             <h1>{{pageTitle}}</h1>
-            <div>My first component!</div>
+            <pm-products></pm-products>
         </div>
-    `
+    `,
+    directives: [ProductListComponent]
 })
 export class AppComponent {
 
