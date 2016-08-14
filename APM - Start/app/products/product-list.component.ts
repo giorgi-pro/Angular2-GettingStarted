730ea98dbdf802c6193b62179893e720f222d74a
Giorgi Zautashvili <Giorgi Zautashvili>
2016-08-14 15:36:56 +0200

    custom pipes and interfaces

--- APM - Start/app/products/product-list.component.ts
+++ APM - Start/app/products/product-list.component.ts
@@ -1,19 +1,23 @@
-import { Component } from 'angular2/core'
+import { Component, OnInit } from 'angular2/core'
+import { IProduct } from './product'
+import { ProductFilterPipe } from './product-filter.pipe'
 
 @Component({
 
     selector: 'pm-products',
-    templateUrl: 'app/products/product-list.component.html'
+    templateUrl: 'app/products/product-list.component.html',
+    styleUrls: ['app/products/product-list.component.css'],
+    pipes: [ProductFilterPipe]
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
 
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
+    listFilter: string;
 
-    products: any[] = [
+    products: IProduct[] = [
         {
         "productId": 1,
         "productName": "Leaf Rake",
@@ -35,6 +39,11 @@ export class ProductListComponent {
         "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
     }
     ];
+
+    ngOnInit(): void {
+
+        console.dir('init');
+    }
 
     toggleImage(): void {
 
