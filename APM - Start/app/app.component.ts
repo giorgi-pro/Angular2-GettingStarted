34f0c2f958a42110479ff9d2049da6ef2dd8a430
Giorgi Zautashvili <Giorgi Zautashvili>
2016-08-21 19:38:29 +0200

    reactive extensions

--- APM - Start/app/app.component.ts
+++ APM - Start/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from 'angular2/core'
+import { HTTP_PROVIDERS } from 'angular2/http'
+import 'rxjs/Rx'
 import { ProductListComponent } from './products/product-list.component'
 import { ProductService } from './services/product.service'
 
@@ -12,7 +14,7 @@ import { ProductService } from './services/product.service'
         </div>
     `,
     directives: [ProductListComponent],
-    providers: [ProductService]
+    providers: [ProductService, HTTP_PROVIDERS]
 })
 export class AppComponent {
 
--- APM - Start/app/products/product-list.component.ts
+++ APM - Start/app/products/product-list.component.ts
@@ -20,6 +20,7 @@ export class ProductListComponent implements OnInit {
     showImage: boolean = false;
     listFilter: string;
     products: IProduct[];
+    errorMessage: any;
 
     constructor(private _productService: ProductService) {
 
@@ -27,7 +28,9 @@ export class ProductListComponent implements OnInit {
 
     ngOnInit(): void {
 
-        this.products = this._productService.getProducts();
+        this._productService.getProducts()
+                            .subscribe(products => this.products = products,
+                            error => this.errorMessage = <any>error);
     }
 
     toggleImage(): void {
