cae37022c69a385ab9f4e151863064aef9a29d8c
Giorgi Zautashvili <Giorgi Zautashvili>
2016-09-02 23:01:16 +0200

    configure routing

--- APM - Start/app/app.component.ts
+++ APM - Start/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from 'angular2/core'
 import { HTTP_PROVIDERS } from 'angular2/http'
+import { ROUTER_PROVIDERS, RouteConfig } from 'angular2/router'
 import 'rxjs/Rx'
 import { ProductListComponent } from './products/product-list.component'
 import { ProductService } from './services/product.service'
+import { WelcomeComponent } from './home/welcome.component'
 
 @Component({
 
@@ -14,8 +16,15 @@ import { ProductService } from './services/product.service'
         </div>
     `,
     directives: [ProductListComponent],
-    providers: [ProductService, HTTP_PROVIDERS]
+    providers: [ProductService, HTTP_PROVIDERS, ROUTER_PROVIDERS]
 })
+@RouteConfig([{
+
+    path: '/welcome', name: 'Welcome', component: WelcomeComponent, useAsDefault: true
+}, {
+    
+    path: '/products', name: 'products', component: ProductListComponent
+}])
 export class AppComponent {
 
     pageTitle: string = 'Acme product management';
